2874b7867e330bc122deeed876aec4c32eeaf172
kiejibi <kaekie2004@gmail.com>
2023-12-01 14:07:33 +0700

    first commit

---
+++ sss.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int N;
+	cout << "Enter the number of student: ";
+	cin >> N;
+	
+	string name[N];
+	int age[N];
+	cout << "Name of student = \n";
+    
+	cout << "Age of student = ";
+	
+	int key;
+	cout << "--------------------------------------\n";
+	cout << "Enter an age to search: ";
+	cin >> key;
+	cout << "--------------------------------------\n";
+	
+
+	
+	cout << "--------------------------------------\n";
+	
+	return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "x0";
+    return 0;
+}
